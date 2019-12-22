27cf0f8600d7e890a5458d2f2605e1ca26c3faf1
nao18 <kyahyo17@yahoo.co.jp>
2019-12-23 06:31:48 +0900

    [add]migrationファイルを追加

---
+++ database/migrations/2019_12_22_202506_create_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->string('id', 255)->primary()->unique();
+            $table->integer('user_id');
+            $table->string('filename', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
